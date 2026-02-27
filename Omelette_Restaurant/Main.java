1600669d4a228cc49a251eebc5ae97bacdc50e72
Monesh-11 <moneshharish2357@gmail.com>
2026-02-27 10:36:57 +0530

    pattern matching

---
+++ Omelette_Restaurant/Main.java
@@ -0,0 +1,14 @@
+import java.util.*;
+class Main{
+    public static void main(String[]args){
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int exp=sc.nextInt();
+        int[]buy=new int[n];
+        int[]used=new int[n];
+        for(int i=0;i<n;i++)buy[i]=sc.nextInt();
+        for(int i=0;i<n;i++)used[i]=sc.nextInt();
+        
+
+    }
+}
